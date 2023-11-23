20bd20a59cda0e122acdd7dcfa3fccd3d088d8e7
Chris O'Neil <chriso83@protonmail.com>
2023-11-23 19:44:52 +0000

    feat: support `sn_node_rpc_client` release type
    The new binary is added to the available release types.
    
    The addition of this binary also caused the incorrect latest version to be returned for `sn_node`
    because it was doing a starts-with match. The match was changed to compare the whole crate name.

--- tests/test_get_latest_version.rs
+++ tests/test_get_latest_version.rs
@@ -37,6 +37,17 @@ async fn should_get_latest_version_of_safenode() {
 }
 
 #[tokio::test]
+async fn should_get_latest_version_of_safenode_rpc_client() {
+    let release_type = ReleaseType::SafenodeRpcClient;
+    let release_repo = <dyn SafeReleaseRepositoryInterface>::default_config();
+    let version = release_repo
+        .get_latest_version(&release_type)
+        .await
+        .unwrap();
+    assert!(valid_semver_format(&version));
+}
+
+#[tokio::test]
 async fn should_get_latest_version_of_testnet() {
     let release_type = ReleaseType::Testnet;
     let release_repo = <dyn SafeReleaseRepositoryInterface>::default_config();
